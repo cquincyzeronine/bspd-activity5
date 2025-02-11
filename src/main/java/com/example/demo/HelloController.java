286c2f367a4e72d84e884a196127cddde4967868
cquincyzeronine <cquincy09@gmail.com>
2025-02-11 10:21:36 +0800

    included compiled springboot

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, Dockerized Spring Boot!";
+    }
+
+    @GetMapping("/api1")
+    public String api1() {
+        return "api1";
+    }
+
+
+}
